c5f13efa27ee88f66a6eeb0ab7cf8cf84c753e00
Pavel Kuropatkin <pasha.rgmu@gmail.com>
2018-03-22 10:11:31 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# amps-blog-easy-http-auth
-Artifacts for the 2018 blog article on easy HTTP authentication and entitlements.
+## amps-blog-easy-http-auth
+
+This repository contains artifacts for the 2018 blog article on easy HTTP authentication and entitlements.
+
+This is a simple project that demonstrates how to use AMPS with
+the `libamps_http_entitlement` module in order to provide authentication
+and/or entitlements via web service.
+
+A detailed description of the `libamps_http_entitlement` module is available here: http://devnull.crankuptheamps.com/documentation/html/5.2.0.0/user-guide/html/chapters/auxiliary_modules.html#configuring-amps-to-use-web-service-authentication-and-entitlements
+
+
+### Requirements
+
+- AMPS Server 5.2 or higher
+- Python 2.7 or higher
+
+
+### Quick Start
+
+- Start the AMPS Server using the configuration file:
+
+    ```bash
+    <PATH_TO_AMPS_BINARY>/ampServer config.xml
+    ```
+
+- Start the Python Flask application (in the `auth` directory):
+
+    ```
+    python main.py
+    ```
+
+- Open the Galvanometer Web interface with your favorite browser. It's available at this URL: `http://localhost:8085`
+  In the login/password window enter: `mylogin` and `mypass`
+
+- Try to subscribe to a topic using `Spark`:
+
+    ```bash
+    <PATH_TO_AMPS_BINARY>/spark subscribe -server mylogin:mypass@localhost:9007 -topic foo -type json
+    ```
+
+
+### Miscellaneous
+
+Want to know more about how AMPS can help you build great applications? Still having trouble getting AMPS to play your tune? For help or questions, send us a note at support@crankuptheamps.com.
---
+++ auth/main.py
@@ -0,0 +1,67 @@
+"""
+Basic Authentication code is based on the code snippet by
+Armin Ronacher: http://flask.pocoo.org/snippets/8/
+"""
+
+from flask import Flask
+from flask import request, Response
+from functools import wraps
+
+
+# create the flask app example
+app = Flask(__name__)
+
+
+def check_auth(username, password):
+    """This function is called to check if a username/password is valid."""
+    return username == 'mylogin' and password == 'mypass'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="AMPS Authentication"'}
+    )
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route('/<username>')
+@requires_auth
+def permission_document(username):
+    return """
+        {
+          "logon": true,
+          "replication-logon": true,
+          "topic": [{
+              "topic": ".*",
+              "read": true,
+              "write": true
+            }],
+          "admin": [{
+              "topic": "/amps/administrator",
+              "read": true,
+              "write": false
+            }, {
+              "topic": ".*",
+              "read": true,
+              "write": false
+            }]
+        }
+    """
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8500)
+
